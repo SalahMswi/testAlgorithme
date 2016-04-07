2d92b170d6445b185c9696a7cebc98e86794e494
SalahMswi <salah.almoussawi@viseo.com>
2016-04-07 10:36:29 +0200

    Update RenduMonnaieTest.java

--- RenduMonnaieTest.java
+++ RenduMonnaieTest.java
@@ -9,15 +9,17 @@ import junit.framework.TestCase;
 public class RenduMonnaieTest extends TestCase{
 
 	public void testCalculer() throws Exception {
-		RenduMonnaie rm=new RenduMonnaie();
+	RenduMonnaie rm=new RenduMonnaie();
 		List<Float> ltest=new LinkedList<Float>();
+		Map<Float,Integer> maptest=new LinkedHashMap<Float,Integer>();		//ltest.add(0.0f);
+		
 		//ltest.add(0.0f);
-		ltest.add(0.0f);
-
+		maptest.put(2f, 2);
+		maptest.put(1f, 1);
 		//assertEquals(0.0f,rm.renvoi_monnaie(1.5f));
 		
 		//assertEquals(ltest,rm.renvoi_monnaie_list(5f));
-		assertEquals(ltest,rm.renvoi_monnaie_map(8.5f));
+		assertEquals(maptest,rm.renvoi_monnaie_map(7.5f));
 
 		
 	}
