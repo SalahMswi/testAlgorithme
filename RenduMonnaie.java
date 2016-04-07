1344fa6e8f3e47edc7c2419124c654072b5d3a46
SalahMswi <salah.almoussawi@viseo.com>
2016-04-07 19:32:51 +0200

    Update RenduMonnaie.java

--- RenduMonnaie.java
+++ RenduMonnaie.java
@@ -1,36 +1,69 @@
-package pour_tests;
+package algotest1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class RenduMonnaie {
 
-	private float prix=2f;
+	private static float prix=2f;
 	private float[] monnaies={2f,1f,0.5f,0.1f};
+	private static Map<Float,Integer> caisse=new HashMap<Float,Integer>();
 
 	public RenduMonnaie(){
 	}
 	
-	//pour cette m�thode je peux utiliser while � la place de if
+	//cette méthode rempli la caisse par le test junit parmétrisé
+	//j'ai céer cette méthode pour essayer de remplir la caisse par test parmétrisé
+	//mais elle marche pas car à chaque fois je crer un nouveau objet alors nouvelle caisse
+public static void remplir_caisse(Float monnaie,Integer nombre_monnaie){
+		
+	boolean exist=false;
+	for(Map.Entry<Float, Integer> en:caisse.entrySet()){
+		if(caisse.containsKey(monnaie))
+		{
+			en.setValue(nombre_monnaie);
+			exist=true;
+		}
+		
+	}
+	if(exist==false)
+		caisse.put(monnaie, nombre_monnaie);
+	}
+
+	//pour cette méthode je peux utiliser while à la place de if
 	//Integer i=m.getValue();
 	//i--;
 	//m.setValue(i);
-public Map<Float, Integer> renvoi_monnaie_map(float argent_depose){
-	Map<Float,Integer> mp=new HashMap<Float,Integer>();
-	Map<Float,Integer> mp_de_monnaies=new HashMap<Float,Integer>();
-
+public static Map<Float, Integer> renvoi_monnaie_map(float argent_depose,Map<Float,Integer> mp){
+	//Map<Float,Integer> mp=new HashMap<Float,Integer>();
+	Map<Float,Integer> mp_de_monnaies=new LinkedHashMap<Float,Integer>();
+/*
 	mp.put(2f, 2);
-	mp.put(1f, 5);
-	mp.put(0.5f, 1);
-	
+	mp.put(1f, 4);
+	mp.put(0.5f, 2);
+	mp.put(0.1f, 10);
+*/
 		float reste=argent_depose-prix;
-		
+		float rest_division=0.0f;
 		for(Map.Entry<Float , Integer> en:mp.entrySet()){
 			if(en.getValue()!=null){
 			if(reste>=en.getKey()){
-				float rest_division=reste%en.getKey();
+				rest_division=reste%en.getKey();
+				String str=String.valueOf(rest_division);
+				int lg=str.length();
+				if(lg>3){ 
+					//ici j'avais un problème qu'il prend le reste en float ex:
+					//si 6.2/2 alors le reste réel est 0.2 par contre il me donnait 0.1999
+					//du coup il faut convertie vers double et puisque mon reste est float
+					//alors pr que je puisse le mettre dans ma variable je le convertit en float
+					double f= (double)rest_division;
+					double d=Math.round(f*100.0)/100.0;
+					float f1=(float) d;
+					rest_division=f1;
+				}
 				int quotient=(int) (reste/en.getKey());
 				
 				if(rest_division==0f){
@@ -38,7 +71,9 @@ public Map<Float, Integer> renvoi_monnaie_map(float argent_depose){
 					if(a>=0)//pr verifier si contient assez de cette monnaie
 					{
 						mp_de_monnaies.put(en.getKey(), quotient);
-					en.setValue(0);
+					en.setValue(a);//il y a un problème là
+					
+					
 					break;
 					}
 					else{
@@ -52,6 +87,7 @@ public Map<Float, Integer> renvoi_monnaie_map(float argent_depose){
 					{
 						mp_de_monnaies.put(en.getKey(), quotient);
 					en.setValue(0);//je vide la caisse de cette monnaie
+					reste=rest_division;
 					}
 					else{
 						mp_de_monnaies.put(en.getKey(), en.getValue());
